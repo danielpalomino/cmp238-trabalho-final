302e4e5c140b763007ecc4354f60f71fce2beac3
Felipe Sampaio <felsamps@gmail.com>
2011-07-11 21:29:59 -0300

    Instanciacao dos calculadores de mediana no datapath

--- arquitetura/data_path.vhd
+++ arquitetura/data_path.vhd
@@ -2,16 +2,33 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+LIBRARY work;
+USE work.VECTOR_LIBRARY.all;
 
 ENTITY data_path IS
-	PORT (
-		
-		);
+	PORT (		
+	);
 END data_path;
 
 ARCHITECTURE comportamento OF data_path IS
 
+	SIGNAL x_a, x_b, x_c, x_s : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL x_a, x_b, x_c, x_s : STD_LOGIC_VECTOR(7 DOWNTO 0);
+
 BEGIN
 
+	MEDIANA_X: mediana PORT MAP (
+		a => x_a,
+		b => x_b,
+		c => x_c,
+		s => x_s
+	);
+
+	MEDIANA_Y: mediana PORT MAP (
+		a => y_a,
+		b => y_b,
+		c => y_c,
+		s => y_s
+	);
 
 END comportamento;
