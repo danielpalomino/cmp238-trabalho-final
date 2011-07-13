0b3257351f65059f68695cb5ccbd14431401d38d
Felipe Sampaio <felsamps@gmail.com>
2011-07-13 00:59:18 -0300

    Registrador para o tipo motion_vector

---
+++ arquitetura/reg_mv.vhd
@@ -0,0 +1,33 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+LIBRARY work;
+USE work.VECTOR_LIBRARY.ALL;
+
+ENTITY reg_mv IS
+	PORT (
+		clk, reset, carga : IN STD_LOGIC;
+		d : IN motion_vector;
+		q : OUT motion_vector
+	);
+END reg_mv;
+
+ARCHITECTURE comportamento OF reg_mv IS
+
+BEGIN
+
+	PROCESS(clk, reset)
+	BEGIN
+		IF reset = '1' THEN
+			q <= (OTHERS => '0');
+		ELSIF clk'EVENT AND clk = '1' THEN
+			IF carga = '1' THEN
+				q <= d;
+			ELSE
+				q <= q;
+			END IF;
+		END IF;
+	END PROCESS;	
+
+END comportamento;
